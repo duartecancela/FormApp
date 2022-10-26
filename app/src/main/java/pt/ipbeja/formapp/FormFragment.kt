caa7417446f9bf3c3c3cc54a7c7aa7e468a96a80
Duarte Cancela <duartecancela@gmail.com>
2022-10-26 22:13:37 +0100

    update result fragments, add args and pass spinner data

--- app/src/main/java/pt/ipbeja/formapp/FormFragment.kt
+++ app/src/main/java/pt/ipbeja/formapp/FormFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.get
 import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.NonDisposableHandle.parent
 import pt.ipbeja.formapp.databinding.FragmentFormBinding
 import kotlin.math.absoluteValue
 
@@ -20,9 +26,41 @@ class FormFragment : Fragment() {
 
         this.binding = FragmentFormBinding.inflate(inflater)
 
+        val name = this.binding.editTextPersonName.text
+        var nationality : String? = null
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        this.activity?.let {
+            ArrayAdapter.createFromResource(
+                it.applicationContext,
+                R.array.nationality,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                this.binding.spinnerNationality.adapter = adapter
+            }
+        }
+
+        // when spinner is selected
+        this.binding.spinnerNationality.onItemSelectedListener = object :
+        AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                nationality = binding.spinnerNationality.getItemAtPosition(p2).toString()
+                Toast.makeText(activity,nationality, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
 
         this.binding.buttonResult.setOnClickListener {
-            findNavController().navigate(FormFragmentDirections.actionFormFragmentToResultFragment())
+            findNavController()
+                .navigate(FormFragmentDirections
+                    .actionFormFragmentToResultFragment(name.toString(), nationality.toString()))
         }
 
         // Inflate the layout for this fragment
--- app/src/main/java/pt/ipbeja/formapp/ResultFragment.kt
+++ app/src/main/java/pt/ipbeja/formapp/ResultFragment.kt
@@ -5,22 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentResultOwner
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import pt.ipbeja.formapp.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
 
     private lateinit var binding : FragmentResultBinding
+    private val args: ResultFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        val personName = args.personName
+        val nationality =args.nationality
+
         this.binding = FragmentResultBinding.inflate(inflater)
 
+        this.binding.textViewPersonName.text = personName
+        this.binding.textViewNationality.text = nationality
+
         this.binding.buttonBack.setOnClickListener {
-            findNavController().popBackStack()
+            findNavController().popBackStack(R.id.startFragment, false)
         }
 
         // Inflate the layout for this fragment
