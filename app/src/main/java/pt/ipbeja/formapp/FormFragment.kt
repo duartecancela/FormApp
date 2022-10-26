f0370081e815b8cb30567f7d72a3521d9940e82d
duartecancela <duartecancela@gmail.com>
2022-10-26 16:36:43 +0100

    update fragments

--- app/src/main/java/pt/ipbeja/formapp/FormFragment.kt
+++ app/src/main/java/pt/ipbeja/formapp/FormFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.ipbeja.formapp.databinding.FragmentFormBinding
 
 class FormFragment : Fragment() {
 
+    private lateinit var binding : FragmentFormBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        this.binding = FragmentFormBinding.inflate(inflater)
+
+        this.binding.buttonResult.setOnClickListener {
+            findNavController().navigate(FormFragmentDirections.actionFormFragmentToResultFragment())
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/pt/ipbeja/formapp/ResultFragment.kt
+++ app/src/main/java/pt/ipbeja/formapp/ResultFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.ipbeja.formapp.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
 
+    private lateinit var binding : FragmentResultBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        this.binding = FragmentResultBinding.inflate(inflater)
+
+        this.binding.buttonBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        return binding.root
     }
 
 
